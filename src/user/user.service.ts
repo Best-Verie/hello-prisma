954d2fd6a5a22721da87848a2e9ad4f1817cdc35
Best-Verie <irabestverie2016@gmail.com>
2022-06-07 21:54:06 +0200

    feat: create user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: string): Promise<User> {
+    return await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async create(userData: CreateUserDto): Promise<User> {
+    return await this.prisma.user.create({
+      data: userData,
+    });
+  }
+}
